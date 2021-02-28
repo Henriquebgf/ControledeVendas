7daf5b3a9f520f56fdb631927d0205f41ebe1780
Luis Henrique Barbosa Gomes Filho <l.henrique.b.g.filho@gmail.com>
2021-02-28 20:08:24 -0300

    Criação do projeto

---
+++ src/main/java/br/edu/iff/ControledeVendas/ControledeVendasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ControledeVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControledeVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControledeVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iff/ControledeVendas/ControledeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ControledeVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControledeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
